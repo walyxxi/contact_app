c7845987ced766ca3b8d4b1abcdfcef30f8d0329
walyxxi <waliyulardy@gmail.com>
2020-09-16 08:40:56 +0700

    [ADD] form add & edit componrnts

--- src/App.js
+++ src/App.js
@@ -29,18 +29,21 @@ const IconDelete = styled(MdDelete)`
 
 const App = () => {
   const [showFormAdd, setShowFormAdd] = useState(false);
+  const [dataEdit, setDataEdit] = useState();
 
   const contact = [
     {
       id: "93ad6070-c92b-11e8b02f",
-      name: "Bilbao Baggins",
+      first_name: "Bilbao",
+      last_name: "Baggins",
       age: 110,
       photo:
         "http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550",
     },
     {
       id: "36rd833d-4fsd-fst43d33",
-      name: "Salman Khan",
+      first_name: "Salman",
+      last_name: "Khan",
       age: 57,
       photo:
         "https://www.kashmirpen.com/wp-content/uploads/2020/09/download-44-289x174.jpg",
@@ -49,6 +52,14 @@ const App = () => {
 
   const handleShowFormAdd = () => {
     setShowFormAdd(!showFormAdd);
+  };
+
+  const handleShowFormEdit = (data) => {
+    setDataEdit(data);
+  };
+
+  const handleCloseFormEdit = () => {
+    setDataEdit();
   };
 
   return (
@@ -71,13 +82,19 @@ const App = () => {
             <div className="ContactList" key={idx}>
               <img height="70px" src={con.photo} alt="" />
               <div className="ContactCaption">
-                <div className="ContactName">{con.name}</div>
+                <div className="ContactName">
+                  {con.first_name} {con.last_name}
+                </div>
                 <div className="ContactAge">Age {con.age} years</div>
                 <div className="ContactAge">ID {con.id}</div>
               </div>
               <div className="ContactEdit">
-                <IconEdit size="14px" color="#cf7500" />
-                <IconDelete size="14px" color="#cf7500" />
+                <IconEdit
+                  onClick={() => handleShowFormEdit(con)}
+                  size="18px"
+                  color="#cf7500"
+                />
+                <IconDelete size="18px" color="#cf7500" />
               </div>
             </div>
           ))}
@@ -85,13 +102,67 @@ const App = () => {
 
       {/* AddBox */}
       {showFormAdd && (
-        <div className="AddBox">
+        <div className="FormEl">
+          <div className="FormTitle">
+            <MdAdd color="#fff" size="20px" />
+            &nbsp;Add Contact
+          </div>
           <div className="FormAdd">
             <div className="InputGroup">
-              <input className="Input" placeholder="ID" />
-              <input className="Input" placeholder="First Name" />
-              <input className="Input" placeholder="Last Name" />
-              <input className="Input" placeholder="Age" />
+              <input className="Input" placeholder="First Name" type="text" />
+              <input className="Input" placeholder="Last Name" type="text" />
+              <input className="Input" placeholder="Age" type="number" />
+              <input className="Input" placeholder="Photo" type="text" />
+            </div>
+            <div className="ButtonGroup">
+              <div onClick={handleShowFormAdd} className="ButtonCancel">
+                Cancel
+              </div>
+              <div className="ButtonSubmit">Submit</div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* EditBox */}
+      {dataEdit && (
+        <div className="FormEl">
+          <div className="FormTitle">
+            <MdAdd color="#fff" size="20px" />
+            &nbsp;Edit Contact
+          </div>
+          <div className="FormAdd">
+            <div className="InputGroup">
+              <input
+                className="Input"
+                placeholder="First Name"
+                type="text"
+                value={dataEdit.first_name}
+              />
+              <input
+                className="Input"
+                placeholder="Last Name"
+                type="text"
+                value={dataEdit.last_name}
+              />
+              <input
+                className="Input"
+                placeholder="Age"
+                type="number"
+                value={dataEdit.age}
+              />
+              <input
+                className="Input"
+                placeholder="Photo"
+                type="text"
+                value={dataEdit.photo}
+              />
+            </div>
+            <div className="ButtonGroup">
+              <div onClick={handleCloseFormEdit} className="ButtonCancel">
+                Cancel
+              </div>
+              <div className="ButtonSubmit">Update</div>
             </div>
           </div>
         </div>
